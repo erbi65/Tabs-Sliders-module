65cc38d5051cdfd0a05d84a1dc47137f1bfa9962
Anderson Grudtner Martins <anderson@ostraining.com>
2016-04-14 21:59:53 -0300

    Fix compatibility with J3.x

--- src/includes/elements/positions.php
+++ src/includes/elements/positions.php
@@ -12,87 +12,45 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-if (version_compare(JVERSION, '1.6.0', 'ge')) {
-    jimport('joomla.form.formfield');
-    class JFormFieldPositions extends JFormField
-    {
-        public $type = 'positions';
 
-        public function getInput()
-        {
-            return JElementPositions::fetchElement($this->name, $this->value, $this->element, $this->options['control']);
-        }
-    }
-}
-
-jimport('joomla.html.parameter.element');
-
-class JElementPositions extends JElement
+jimport('joomla.form.formfield');
+class JFormFieldPositions extends JFormField
 {
+    public $type = 'positions';
 
-    protected $_name = 'positions';
-
-    public function fetchElement ($name, $value, &$node, $control_name)
+    public function getInput()
     {
-        $db = JFactory::getDBO();
+        $db   = JFactory::getDBO();
 
-        if (version_compare(JVERSION, '1.6.0', 'ge')) {
-            $query = 'SELECT DISTINCT(template) AS text, template AS value FROM `#__template_styles` WHERE client_id = 0';
-            $db->setQuery($query);
-        } else {
-            $query = 'SELECT DISTINCT(template) AS text, template AS value FROM `#__templates_menu` WHERE client_id = 0';
-            $db->setQuery($query);
-        }
-
-        $templates = $db->loadObjectList();
-        $query = 'SELECT DISTINCT(position) FROM `#__modules` WHERE client_id = 0';
+        $query = $db->getQuery(true)
+            ->select('DISTINCT template AS text')
+            ->select('template AS value')
+            ->from('#__template_styles')
+            ->where('client_id = 0');
         $db->setQuery($query);
+        $templates = $db->loadObjectList();
 
-        $positions = $db->loadResultArray();
+        $query = $db->getQuery(true)
+            ->select('DISTINCT position')
+            ->from('#__modules')
+            ->where('client_id = 0');
+        $db->setQuery($query);
+        $positions = $db->loadColumn();
         $positions = (is_array($positions)) ? $positions : array();
 
         for ($i = 0, $n = count($templates); $i < $n; $i++) {
-            $path = JPATH_SITE.DS.'templates'.DS.$templates[$i]->value;
-            $xml = JFactory::getXMLParser('Simple');
-            if ($xml->loadFile($path.DS.'templateDetails.xml')) {
-                $p = $xml->document->getElementByPath('positions');
-                if (is_a($p, 'JSimpleXMLElement') && count($p->children())) {
-                    foreach ($p->children() as $child) {
-                        if (!in_array($child->data(), $positions)) {
-                            $positions[] = $child->data();
-                        }
+            $path         = JPATH_SITE . '/templates/' . $templates[$i]->value;
+            $xmlPath      = $path . '/templateDetails.xml';
+
+            if (file_exists($xmlPath)) {
+                $xml = simplexml_load_file($xmlPath);
+
+                if (isset($xml->positions[0])) {
+                    foreach ($xml->positions[0] as $position) {
+                        $positions[] = (string) $position;
                     }
                 }
             }
-        }
-
-        if (defined('_JLEGACY') && _JLEGACY == '1.0') {
-            $positions[] = 'left';
-            $positions[] = 'right';
-            $positions[] = 'top';
-            $positions[] = 'bottom';
-            $positions[] = 'inset';
-            $positions[] = 'banner';
-            $positions[] = 'header';
-            $positions[] = 'footer';
-            $positions[] = 'newsflash';
-            $positions[] = 'legals';
-            $positions[] = 'pathway';
-            $positions[] = 'breadcrumb';
-            $positions[] = 'user1';
-            $positions[] = 'user2';
-            $positions[] = 'user3';
-            $positions[] = 'user4';
-            $positions[] = 'user5';
-            $positions[] = 'user6';
-            $positions[] = 'user7';
-            $positions[] = 'user8';
-            $positions[] = 'user9';
-            $positions[] = 'advert1';
-            $positions[] = 'advert2';
-            $positions[] = 'advert3';
-            $positions[] = 'debug';
-            $positions[] = 'syndicate';
         }
 
         $positions = array_unique($positions);
@@ -105,9 +63,7 @@ class JElementPositions extends JElement
             }
         }
 
-        $fieldName = version_compare(JVERSION, '1.6.0', 'ge') ? $name : $control_name.'['.$name.']';
-
-        $output = JHTML::_('select.genericlist', $options, $fieldName, 'class="inputbox"', 'id', 'title', $value);
+        $output = JHTML::_('select.genericlist', $options, $this->name, 'class="inputbox"', 'id', 'title', $this->value);
 
         return $output;
     }
